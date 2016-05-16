aa8fa362a5c384ebb5af5da110588f916bf94fb6
ayman abdelghany <aymana.ghany@devfactory.com>
2016-05-16 02:49:49 +0200

    Utility classes should not have public constructors

--- SimpleNoSQL/src/androidTest/java/com/colintmiller/simplenosql/TestUtils.java
+++ SimpleNoSQL/src/androidTest/java/com/colintmiller/simplenosql/TestUtils.java
@@ -8,6 +8,7 @@ import java.util.concurrent.CountDownLatch;
  * Created by cmiller on 8/12/14.
  */
 public class TestUtils {
+    private TestUtils() {}
 
     public static CountDownLatch cleanBucket(String bucket, Context context) {
         final CountDownLatch signal = new CountDownLatch(1);
